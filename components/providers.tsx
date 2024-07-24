b611145953f37935907d3621fff44e17354767a1
Steve <stevedsimkins@gmail.com>
2024-07-24 14:39:59 -0400

    CSP

--- components/providers.tsx
+++ components/providers.tsx
@@ -9,8 +9,8 @@ export default function Providers({ children }: { children: React.ReactNode }) {
       config={{
         appearance: {
           theme: "light",
-          accentColor: "#676FFF",
-          logo: "https://your-logo-url",
+          accentColor: "#000000",
+          logo: "https://dweb.mypinata.cloud/ipfs/bafkreigurw5b63x3yg3mjpqn3nmhaekiqowduck4h2uo2nn6iaobsytvkm/",
         },
         embeddedWallets: {
           createOnLogin: "users-without-wallets",
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,50 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const cspHeader = `
+ default-src 'self';
+    script-src 'self' localhost 'unsafe-eval' 'unsafe-inline' https://challenges.cloudflare.com;
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data: https://www.signets.cloud/faicon.ico https://dweb.mypinata.cloud/;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    child-src https://auth.privy.io https://verify.walletconnect.com https://verify.walletconnect.org;
+    frame-src https://auth.privy.io https://verify.walletconnect.com https://verify.walletconnect.org https://challenges.cloudflare.com;
+    connect-src https://gray-envious-minnow-323.dev-mypinata.cloud/ipfs/* https://dweb.mypinata.cloud/ipfs/* http://localhost:3000/api/verify http://localhost:3000/api/sign http://localhost:3000/api/key https://auth.privy.io/* https://*.rpc.privy.systems wss://relay.walletconnect.com wss://relay.walletconnect.org wss://www.walletlink.org https://www.signets.cloud/api/verify https://www.signets.cloud/api/sign https://www.signets.cloud/api/key https://api.pinata.cloud/pinning/pinFileToIPFS https://www.signets.cloud/*;
+    upgrade-insecure-requests;
+`;
+
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: cspHeader.replace(/\n/g, ""),
+          },
+        ],
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+    ],
+  },
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
+};
 
 export default nextConfig;
