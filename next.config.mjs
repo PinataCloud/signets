6bbf64f615c6d4d64014ff6d2dd0d0ea349b1643
Steve <stevedsimkins@gmail.com>
2024-07-25 10:40:49 -0400

    fixed it lol

--- next.config.mjs
+++ next.config.mjs
@@ -1,34 +1,6 @@
 /** @type {import('next').NextConfig} */
-const cspHeader = `
- default-src 'self';
-    script-src 'self' localhost 'unsafe-eval' 'unsafe-inline' https://challenges.cloudflare.com;
-    style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data: https://www.signets.cloud/faicon.ico https://dweb.mypinata.cloud/;
-    font-src 'self';
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'none';
-    child-src https://auth.privy.io https://verify.walletconnect.com https://verify.walletconnect.org;
-    frame-src https://auth.privy.io https://verify.walletconnect.com https://verify.walletconnect.org https://challenges.cloudflare.com;
-    connect-src https://gray-envious-minnow-323.dev-mypinata.cloud https://dweb.mypinata.cloud http://localhost:3000/api/verify http://localhost:3000/api/sign http://localhost:3000/api/key https://auth.privy.io https://*.rpc.privy.systems wss://relay.walletconnect.com wss://relay.walletconnect.org wss://www.walletlink.org https://www.signets.cloud/api/verify https://www.signets.cloud/api/sign https://www.signets.cloud/api/key https://api.pinata.cloud/pinning/pinFileToIPFS https://www.signets.cloud/* https://api.devpinata.cloud/pinning/pinFileToIPFS https://privy.www.signets.cloud https://explorer-api.walletconnect.com;
-    upgrade-insecure-requests;
-`;
 
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: cspHeader.replace(/\n/g, ""),
-          },
-        ],
-      },
-    ];
-  },
   images: {
     remotePatterns: [
       {
